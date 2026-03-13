03b11a72a9f858f1dc92247401f0d31e9c9f48f8
aniruddhmishra80 <aniruddhmishra2006@gmail.com>
2026-03-13 14:47:54 +0530

    Payment Gateway reconstruction

--- lib/contexts/PlanContext.tsx
+++ lib/contexts/PlanContext.tsx
@@ -1,3 +1,47 @@
+// "use client";
+
+// import { createContext, useContext } from "react";
+
+// interface PlanContextType {
+//   isFreeUser: boolean;
+//   hasProPlan: boolean;
+//   hasEnterprisePlan: boolean;
+// }
+
+// const PlanContext = createContext<PlanContextType | undefined>(undefined);
+
+// interface PlanProviderProps {
+//   children: React.ReactNode;
+//   hasProPlan: boolean;
+//   hasEnterprisePlan: boolean;
+// }
+
+// export function PlanProvider({
+//   children,
+//   hasProPlan,
+//   hasEnterprisePlan,
+// }: PlanProviderProps) {
+//   return (
+//     <PlanContext.Provider
+//       value={{
+//         hasProPlan,
+//         hasEnterprisePlan,
+//         isFreeUser: !hasProPlan && !hasEnterprisePlan,
+//       }}
+//     >
+//       {children}
+//     </PlanContext.Provider>
+//   );
+// }
+
+// export const usePlan = () => {
+//   const context = useContext(PlanContext);
+//   if (context === undefined) {
+//     throw new Error("usePlan needs to be inside the provider");
+//   }
+
+//   return context;
+// };
 "use client";
 
 import { createContext, useContext } from "react";
@@ -18,15 +62,17 @@ interface PlanProviderProps {
 
 export function PlanProvider({
   children,
+  // We keep the props so we don't break the parent component, 
+  // but we are going to ignore them below!
   hasProPlan,
   hasEnterprisePlan,
 }: PlanProviderProps) {
   return (
     <PlanContext.Provider
       value={{
-        hasProPlan,
-        hasEnterprisePlan,
-        isFreeUser: !hasProPlan && !hasEnterprisePlan,
+        hasProPlan: true,        // Force the app to think you are Pro
+        hasEnterprisePlan: true, // Force Enterprise too, just in case!
+        isFreeUser: false,       // Turn off free tier restrictions
       }}
     >
       {children}
